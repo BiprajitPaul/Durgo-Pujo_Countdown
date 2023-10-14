2b2ef85036f67e6d82a2a391f77fa2e73405a71b
Biprajit Paul <138221818+BiprajitPaul@users.noreply.github.com>
2023-10-14 14:22:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+const dest = new Date("oct 21, 2023 10:00:00").getTime();
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let diff = dest - now;
+
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    console.log(days);
+
+    let hours = Math.floor((diff % (1000 * 24 * 60 * 60)) / (1000 * 60 * 60));
+    console.log(hours);
+
+    let Min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    console.log(Min);
+    let Sec = Math.floor((diff % (1000 * 60)) / (1000));
+    console.log(Sec);
+
+    document.querySelector('#days').innerHTML = days;
+    document.querySelector('#Hrs').innerHTML = hours;
+    document.querySelector('#Min').innerHTML = Min;
+    document.querySelector('#sec').innerHTML = Sec;
+},1000);
+
+
+
+
+
+// 1day = 24 hrs
+// 1hr = 60min;
+// 1min = 60sec;
